4f373c44aabb6382f4932944394a5d7f6d69505e
Tom Rooman <tom.rooman@epitech.eu>
2018-08-17 18:59:19 +0200

    put icon for connexion

--- client/src/components/navbar/index.js
+++ client/src/components/navbar/index.js
@@ -1,5 +1,12 @@
 import './navbar.css';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSignInAlt, faSignOutAlt } from '@fortawesome/free-solid-svg-icons';
 import React, { Component } from 'react';
+
+library.add(faSignInAlt);
+library.add(faSignOutAlt);
+
 
 class NavbarComponent extends Component {
 
@@ -7,52 +14,65 @@ class NavbarComponent extends Component {
     if (this.props.url !== undefined) {
       let current = this.props.url.url.split('/')[0];
       return (
-        <nav className="navbar navbar-expand-lg stocker-nav">
+        <nav className="navbar navbar-expand navbar-fixed-top navbar-light bg-light stocker-nav">
           <a className="navbar-brand" href='/'>Stockerplace</a>
           <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
-          <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
 
-            {this.props.user ?
-              <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
-                <li className={current !== 'social' ? "nav-item dropdown active" : "nav-item dropdown"}>
-                  <a className="nav-link dropdown-toggle" href="/" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    Cloud
+          {this.props.user ?
+            <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
+              <li className={current !== 'social' ? "nav-item dropdown active" : "nav-item dropdown"}>
+                <a className="nav-link dropdown-toggle" href="/" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                  Cloud
               </a>
-                  <div className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                    <a className="dropdown-item" href="/cloud">Mes fichiers</a>
-                    <a className="dropdown-item" href="/cloud/upload">Uploader un fichier</a>
+                <div className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                  <a className="dropdown-item" href="/cloud">Mes fichiers</a>
+                  <a className="dropdown-item" href="/cloud/upload">Uploader un fichier</a>
+                </div>
+              </li>
+              <li className={current === 'social' ? "nav-item active" : "nav-item"}>
+                <a className="nav-link" href="/social">Social</a>
+              </li>
+            </ul>
+            :
+            <ul className="navbar-nav mr-auto mt-2 mt-lg-0"></ul>}
+
+          {this.props.user ?
+            <div>
+              <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
+                <li className="nav-item dropdown">
+                  <a className="nav-link dropdown-toggle" href="/" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    {this.props.user.pseudo}
+                  </a>
+                  <div className="dropdown-menu dropdown-profile" aria-labelledby="navbarDropdownMenuLink">
+                    <a className="dropdown-item" href={`/user/${this.props.user.id}`}>Profile</a>
+                    <div className="dropdown-divider"></div>
+                    <a className="dropdown-item nav-disconnect" onClick={this.props.disconnect}>Se déconnecter</a>
                   </div>
                 </li>
-                <li className={current === 'social' ? "nav-item active" : "nav-item"}>
-                  <a className="nav-link" href="/social">Social</a>
+              </ul>
+            </div> :
+            <div>
+              <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
+                <li className="nav-item dropdown">
+                  <a className="nav-link dropdown-toggle" href="/" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Choix
+                </a>
+                  <div className="dropdown-menu dropdown-profile" aria-labelledby="navbarDropdownMenuLink">
+                    <a className="dropdown-item subscribe_button" onClick={this.props.subscribe_modal}>
+                      S'inscrire
+              <FontAwesomeIcon icon="times" />
+                    </a>
+                    <div className="dropdown-divider"></div>
+                    <a className="dropdown-item signin_button" onClick={this.props.connect_modal}>
+                      Se connecter
+              <FontAwesomeIcon icon="sign-in-alt" />
+                    </a>                  </div>
                 </li>
               </ul>
-              :
-              <ul className="navbar-nav mr-auto mt-2 mt-lg-0"></ul>}
-
-            {this.props.user ?
-              <div>
-                <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
-                  <li className="nav-item dropdown">
-                    <a className="nav-link dropdown-toggle" href="/" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                      {this.props.user.pseudo}
-                    </a>
-                    <div className="dropdown-menu dropdown-profile" aria-labelledby="navbarDropdownMenuLink">
-                      <a className="dropdown-item" href={`/user/${this.props.user.id}`}>Profile</a>
-                      <div className="dropdown-divider"></div>
-                      <a className="dropdown-item nav-disconnect" onClick={this.props.disconnect}>Se déconnecter</a>
-                    </div>
-                  </li>
-                </ul>
-              </div> :
-              <div>
-                <button className="btn btn-outline-info my-2 my-sm-0" onClick={this.props.connect_modal}>Se connecter</button>
-                <button className="btn btn-outline-success my-2 my-sm-0" onClick={this.props.subscribe_modal}>S'inscrire</button>
-              </div>
-            }
-          </div>
+            </div>
+          }
         </nav>
       );
     }
